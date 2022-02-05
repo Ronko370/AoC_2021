c262a0b6c32b29088fe71a593602a29e8f082a4a
Dániel Kolozsvári <kolozsvari.dl@gmail.com>
2022-02-05 17:33:50 +0100

    Day 18 - part 2

--- 18.cc
+++ 18.cc
@@ -13,6 +13,15 @@ public:
     Number(const Number& _left, const Number& _right) :
         left(new Number(_left)), right(new Number(_right)){}
 
+    Number(const Number& n) {
+        if(n.value) {
+            value = new int(*n.value);
+        } else if(n.left) {
+            left = new Number(*n.left);
+            right = new Number(*n.right);
+        }
+    }
+
     Number(const std::string& s) {
         std::stack<Number*> numbers;
         for(const auto& c : s) { {}
@@ -30,10 +39,17 @@ public:
         left = numbers.top()->left;
         right = numbers.top()->right;
         value = numbers.top()->value;
-        delete numbers.top();
     }
 
     Number(){}
+
+    int magnitude() const {
+        if(value) {
+            return *value;
+        }
+
+        return 3*left->magnitude() + 2*right->magnitude();
+    }
 
     friend std::ostream& operator<<(std::ostream& o, const Number& n) {
         if(n.value) {
@@ -45,6 +61,16 @@ public:
         return o;
     }
 
+    Number& operator=(const Number& n) {
+        if(n.value) {
+            value = new int(*n.value);
+        } else if(n.left) {
+            left = new Number(*n.left);
+            right = new Number(*n.right);
+        }
+        return *this;
+    }
+
     Number operator+(const Number& n) {
         if(!left) {
             return n;
@@ -52,20 +78,19 @@ public:
             Number result (*this, n);
             for(bool updated = true; updated;) {
                 updated = explode(&result) || split(&result);
+
             }
             return result;
         }
     }
 
-    int magnitude() const {
-        if(value) {
-            return *value;
-        }
+    auto operator<=>(const Number& rhs) const = default;
 
-        return 3*left->magnitude() + 2*right->magnitude();
+    ~Number(){
+        delete left, right, value;
     }
 
-//private:
+private:
     Number* getLeaf (Number* n, bool right = false) {
         Number* first = right ? n->right : n->left;
         Number* second = right ? n->left : n->right;
@@ -83,6 +108,7 @@ public:
         if (depth == 4 && n->left && n->right) {
             if(n->value) {
                 delete n->value;
+                n-> value = nullptr;
             }
             n->value = new int(0);
             return n;
@@ -106,7 +132,6 @@ public:
                 }
             }
         }
-
         return _n;
     }
 
@@ -131,34 +156,28 @@ public:
 };
 
 int main() {
-   // std::ifstream f("18_input.txt");
-    std::ifstream f("temp.txt");
+    std::ifstream f("18_input.txt");
+   // std::ifstream f("temp.txt");
+    std::vector<Number> numbers;
     Number sum;
     for(std::string s; f >> s;) {
         Number n(s);
-        std::cout << n << std::endl;
-        std::cout << n.magnitude() << std::endl;
         sum = sum + n;
+        numbers.push_back(n);
     }
 
     std::cout << sum << std::endl;
-
     std::cout << sum.magnitude() << std::endl;
 
-/*
-    std::vector<Number> numbers;
-    for(std::string s; f >> s;) {
-        numbers.push_back(Number(s));
+    int max_magnitude = 0;
+    for(const auto& n1 : numbers) {
+        for(const auto& n2: numbers) {
+            if(n1 != n2) {
+                max_magnitude =
+                    std::max((Number(n1) + n2).magnitude(), max_magnitude);
+            }
+        }
     }
 
-
-    Number sum = std::accumulate(
-        numbers.begin(), numbers.end(), Number());
-    std::cout << sum << std::endl;
-    std::cout << "------------------" << std::endl;
-    for(auto& n : numbers) {
-        std::cout << n << std::endl;
-        std::cout << "--------" << std::endl;
-    }
-*/
+    std::cout << "Maximum magnitude: " << max_magnitude << std::endl;
 }
